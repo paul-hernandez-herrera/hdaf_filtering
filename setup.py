ae54f534250502926b5ae18c1e1b2c74ac180fb1
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2022-08-17 23:28:06 -0500

    Modfied version of the requirements to make it work with python 3.7

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hdaf_filter',  
-    version = '0.1.0',
+    version = '0.1.1',
     description='Fourier filtering using the Hermite Distributed Approximation Functions as the low-pass filter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/paul-hernandez-herrera/hdaf_filtering',
     author='Paúl Hernández-Herrera',
     author_email='paul.hernandez@ibt.unam.mx',
     license='BSD 3-clause',
     packages=['hdaf_filter'],
-    install_requires=["matplotlib>=3.5.2", 
-        "numpy>=1.23.1",
-        "setuptools>=58.1.0",
-        "tifffile>=2022.7.28"],
-
+    install_requires=["matplotlib>=3.2.2", 
+        "numpy>=1.21.6",
+        "setuptools>=57.4.0",
+        "tifffile>=2021.11.2"],
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',  
